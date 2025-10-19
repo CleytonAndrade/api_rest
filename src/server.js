62d369669bc58a3318768e58dd669e95587dd0aa
CleytonAndrade <clyadr@gmail.com>
2025-10-19 14:09:21 -0300

    ✨ feat(server): configurar porta do servidor com fallback
    - Adiciona suporte para variável de ambiente APP_PORT
    - Define 3001 como fallback caso APP_PORT não esteja definido
    - Adiciona log para informar a porta em que o servidor está rodando

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
 import app from './app.js';
 
-app.listen(process.env.APP_PORT);
+const port = process.env.APP_PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
+  console.log(`CTRL + Clique em http://localhost:${port}`);
+});
